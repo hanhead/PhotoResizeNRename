8556550e76990815b79bed7730af7dd14138978e
james <hanhead@gmail.com>
2023-02-26 08:25:47 -0800

    first commit

--- PhotoResizeNRename/Form1.Designer.cs
+++ PhotoResizeNRename/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PhotoResizeNRename
 {
-    partial class Form1
+    partial class frmPhotoResizeNRename
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPhotoResizeNRename));
+            folderBrowserDialogForOriginalImageFolder = new FolderBrowserDialog();
+            button1 = new Button();
+            listView1 = new ListView();
+            imageList1 = new ImageList(components);
+            textBox1 = new TextBox();
+            button2 = new Button();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label3 = new Label();
+            button3 = new Button();
+            folderBrowserDialogConvertedImageFolder = new FolderBrowserDialog();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(341, 31);
+            button1.TabIndex = 0;
+            button1.Text = "Select Original Image Folder";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listView1
+            // 
+            tableLayoutPanel1.SetColumnSpan(listView1, 2);
+            listView1.Dock = DockStyle.Fill;
+            listView1.LargeImageList = imageList1;
+            listView1.Location = new Point(3, 40);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(1149, 497);
+            listView1.SmallImageList = imageList1;
+            listView1.TabIndex = 1;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth8Bit;
+            imageList1.ImageSize = new Size(50, 50);
+            imageList1.TransparentColor = Color.Transparent;
+            // 
+            // textBox1
+            // 
+            textBox1.Dock = DockStyle.Fill;
+            textBox1.Location = new Point(3, 543);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(956, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(965, 543);
+            button2.Name = "button2";
+            button2.Size = new Size(140, 25);
+            button2.TabIndex = 3;
+            button2.Text = "Change Folder";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 193F));
+            tableLayoutPanel1.Controls.Add(button1, 0, 0);
+            tableLayoutPanel1.Controls.Add(button2, 1, 2);
+            tableLayoutPanel1.Controls.Add(listView1, 0, 1);
+            tableLayoutPanel1.Controls.Add(textBox1, 0, 2);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 3);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.851852F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 93.14815F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 31F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 63F));
+            tableLayoutPanel1.Size = new Size(1155, 635);
+            tableLayoutPanel1.TabIndex = 4;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 5;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 100F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 150F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 150F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 100F));
+            tableLayoutPanel2.Controls.Add(label1, 0, 0);
+            tableLayoutPanel2.Controls.Add(textBox2, 1, 0);
+            tableLayoutPanel2.Controls.Add(textBox3, 2, 0);
+            tableLayoutPanel2.Controls.Add(textBox4, 3, 0);
+            tableLayoutPanel2.Controls.Add(label3, 3, 1);
+            tableLayoutPanel2.Controls.Add(button3, 4, 0);
+            tableLayoutPanel2.Location = new Point(3, 574);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 2;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Size = new Size(956, 49);
+            tableLayoutPanel2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(27, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Size";
+            // 
+            // textBox2
+            // 
+            textBox2.Dock = DockStyle.Fill;
+            textBox2.Location = new Point(103, 3);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(144, 23);
+            textBox2.TabIndex = 1;
+            textBox2.Text = "1024";
+            textBox2.KeyPress += NumericTextBox_KeyPress;
+            // 
+            // textBox3
+            // 
+            textBox3.Dock = DockStyle.Fill;
+            textBox3.Location = new Point(253, 3);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(144, 23);
+            textBox3.TabIndex = 2;
+            textBox3.Text = "1024";
+            textBox3.KeyPress += NumericTextBox_KeyPress;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(403, 3);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(288, 23);
+            textBox4.TabIndex = 3;
+            textBox4.Text = "newimagename_{sequence number}";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(403, 24);
+            label3.Name = "label3";
+            label3.Size = new Size(224, 15);
+            label3.TabIndex = 5;
+            label3.Text = "* {original file name}, {sequence number}";
+            // 
+            // button3
+            // 
+            button3.Dock = DockStyle.Fill;
+            button3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(697, 3);
+            button3.Name = "button3";
+            tableLayoutPanel2.SetRowSpan(button3, 2);
+            button3.Size = new Size(256, 43);
+            button3.TabIndex = 6;
+            button3.Text = "Convert !!";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // frmPhotoResizeNRename
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1155, 635);
+            Controls.Add(tableLayoutPanel1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "frmPhotoResizeNRename";
+            Text = "ImageResizeNRename";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private FolderBrowserDialog folderBrowserDialogForOriginalImageFolder;
+        private Button button1;
+        private ListView listView1;
+        private ImageList imageList1;
+        private TextBox textBox1;
+        private Button button2;
+        private TableLayoutPanel tableLayoutPanel1;
+        private FolderBrowserDialog folderBrowserDialogConvertedImageFolder;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label3;
+        private Button button3;
     }
 }
--- PhotoResizeNRename/Form1.cs
+++ PhotoResizeNRename/Form1.cs
@@ -1,10 +1,383 @@
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+
 namespace PhotoResizeNRename
 {
-    public partial class Form1 : Form
+    public partial class frmPhotoResizeNRename : Form
     {
-        public Form1()
+        public frmPhotoResizeNRename()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialogForOriginalImageFolder.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+
+                string folderPath = folderBrowserDialogForOriginalImageFolder.SelectedPath;
+                textBox1.Text = folderPath + "\\convetedimages\\";
+                string[] imageFiles = Directory.GetFiles(folderPath, "*.*", SearchOption.TopDirectoryOnly)
+                    .Where(file => file.ToLower().EndsWith(".jpg") || file.ToLower().EndsWith(".png") || file.ToLower().EndsWith(".gif") || file.ToLower().EndsWith(".bmp"))
+                    .ToArray();
+                int i = 0;
+                imageList1.Images.Clear();
+                listView1.Items.Clear();
+                foreach (string imageFile in imageFiles)
+                {
+                    Image image = Image.FromFile(imageFile);
+
+                    imageList1.Images.Add(image);
+                    listView1.Items.Add(new ListViewItem(Path.GetFileName(imageFile), i));
+                    i++;
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialogConvertedImageFolder.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialogConvertedImageFolder.SelectedPath;
+            }
+        }
+
+        private void NumericTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string folderPath = folderBrowserDialogForOriginalImageFolder.SelectedPath;
+            if (string.IsNullOrEmpty(folderPath)) { MessageBox.Show("Please select target folder"); return; }
+            textBox1.Text = folderPath + "\\convetedimages\\";
+            string[] imageFiles = Directory.GetFiles(folderPath, "*.*", SearchOption.TopDirectoryOnly)
+                .Where(file => file.ToLower().EndsWith(".jpg") || file.ToLower().EndsWith(".png") || file.ToLower().EndsWith(".gif") || file.ToLower().EndsWith(".bmp"))
+                .ToArray();
+            int seq = 1;
+            string convertedImageFileFolder = Path.GetFullPath(textBox1.Text);
+            foreach (string imageFile in imageFiles)
+            {
+                if (Directory.Exists(convertedImageFileFolder) == false)
+                {
+                    Directory.CreateDirectory(convertedImageFileFolder);
+                }
+                string fileName = Path.GetFileName(imageFile);
+                string newFileName = textBox4.Text.Replace("{sequence number}", seq.ToString()).Replace("{original file name}", fileName.Substring(0, fileName.LastIndexOf("."))) + ".jpg";
+                new ImageResizer().Resize2FileByRecommendJPEG(int.Parse(textBox2.Text), int.Parse(textBox3.Text), imageFile, Path.Combine(convertedImageFileFolder, newFileName));
+                seq++;
+            }
+            MessageBox.Show("Successfully converted!!");
+            Process.Start("explorer.exe", convertedImageFileFolder);
+        }
+    }
+    public class ImageResizer
+    {
+        #region basic resize logic
+        public Point ProportionablyResize(Point orginSize, Point targetSize)
+        {
+            Point Resized = orginSize;
+            if (Resized.X > targetSize.X || Resized.Y > targetSize.Y)
+            {
+                if (Resized.X > Resized.Y)
+                {
+                    if (Resized.X > targetSize.X)
+                    {
+                        Resized = ResizeByX(targetSize, Resized);
+                    }
+                    else
+                    {
+                        Resized = ResizeByY(targetSize, Resized);
+                    }
+                }
+                else
+                {
+                    if (Resized.Y > targetSize.Y)
+                    {
+                        Resized = ResizeByY(targetSize, Resized);
+                    }
+                    else
+                    {
+                        Resized = ResizeByX(targetSize, Resized);
+                    }
+                }
+            }
+            return Resized;
+        }
+
+        private Point ResizeByY(Point targetSize, Point Resized)
+        {
+            Resized.X = (int)((double)Resized.X * ((double)targetSize.Y / (double)Resized.Y));
+            Resized.Y = targetSize.Y;
+            if (Resized.X > targetSize.X)
+            {
+                Resized = ResizeByX(targetSize, Resized);
+            }
+            return Resized;
+        }
+
+        private Point ResizeByX(Point targetSize, Point Resized)
+        {
+            Resized.Y = (int)((double)Resized.Y * ((double)targetSize.X / (double)Resized.X));
+            Resized.X = targetSize.X;
+            if (Resized.Y > targetSize.Y)
+            {
+                Resized = ResizeByY(targetSize, Resized);
+            }
+            return Resized;
+        }
+
+        public Point ProportionablyResize(Point originSize, int targetWidth)
+        {
+            Point Resized = originSize;
+            if (Resized.X > targetWidth)
+            {
+                Resized.Y = (int)(Resized.Y * ((double)targetWidth / Resized.X));
+                Resized.X = targetWidth;
+            }
+            return Resized;
+        }
+
+        //original drawing procedure
+        public Bitmap DrawResizedImage(int Width, int Height, Bitmap Source)
+        {
+            Bitmap _result = new Bitmap(Width, Height);
+            if (_result.PropertyIdList.Contains(0x0112))
+            {
+                int rotationValue = _result.GetPropertyItem(0x0112).Value[0];
+                switch (rotationValue)
+                {
+                    case 1:
+                        break;
+                    case 8:
+                        _result.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                        break;
+                    case 3:
+                        _result.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        break;
+                    case 6:
+                        _result.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        break;
+                }
+            }
+            for (int i = 0; i < Source.PropertyItems.Length; i++)
+            {
+                _result.SetPropertyItem(Source.PropertyItems[i]);
+            }
+            try
+            {
+                using (Graphics g = Graphics.FromImage(_result))
+                {
+                    g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    g.SmoothingMode = SmoothingMode.HighQuality;
+                    g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                    g.CompositingQuality = CompositingQuality.HighQuality;
+                    g.DrawImage(Source, 0, 0, Width, Height);
+                }
+            }
+            catch (Exception _exp)
+            {
+                _result.Dispose();
+                throw _exp;
+            }
+            return _result;
+        }
+
+        public void SaveToFile(string resizedFilePath, ImageFormat ImgFormat, Bitmap Resized, bool RecommendJPEGOptions = false)
+        {
+            if (ImgFormat == ImageFormat.Jpeg && RecommendJPEGOptions)
+            {
+                using (EncoderParameters codecParams = new EncoderParameters(1))
+                {
+                    ImageCodecInfo _imageCodecInfo = ImageCodecInfo.GetImageEncoders().SingleOrDefault(ie => ie.MimeType == "image/jpeg");
+
+                    Encoder qualityEncoder = Encoder.Quality;
+                    codecParams.Param[0] = new EncoderParameter(qualityEncoder, 100L);
+                    Resized.Save(resizedFilePath, _imageCodecInfo, codecParams);
+                }
+            }
+            else
+            {
+                Resized.Save(resizedFilePath, ImgFormat);
+            }
+        }
+
+        #endregion
+
+        #region resize and return Bitmap
+
+        #region from bitmap
+        public Bitmap Resize(int Width, int Height, Bitmap Source, bool Proportionably = true)
+        {
+            Point TargetSize;
+            if (Proportionably)
+                TargetSize = ProportionablyResize((new Point(Source.Width, Source.Height)), (new Point(Width, Height)));
+            else
+                TargetSize = new Point(Width, Height);
+
+            return DrawResizedImage(TargetSize.X, TargetSize.Y, Source);
+        }
+        public Bitmap Resize(int Width, Bitmap Source)
+        {
+            Point TargetSize = ProportionablyResize((new Point(Source.Width, Source.Height)), Width);
+            return DrawResizedImage(TargetSize.X, TargetSize.Y, Source);
+        }
+        #endregion
+
+        #region from file
+        public Bitmap Resize(int Width, int Height, string FilePath, bool Proportionably = true)
+        {
+            Bitmap Resized;
+            using (Bitmap Source = new Bitmap(FilePath))
+            {
+                Resized = Resize(Width, Height, Source, Proportionably);
+            }
+            return Resized;
+        }
+        public Bitmap Resize(int Width, string FilePath)
+        {
+            Bitmap Resized;
+            using (Bitmap Source = new Bitmap(FilePath))
+            {
+                Resized = Resize(Width, Source);
+            }
+            return Resized;
+        }
+        #endregion
+
+        #endregion
+
+        #region resize and return stream
+
+        #region from stream
+
+        public Stream Resize2ImageStream(int Width, Stream imageStream)
+        {
+            Bitmap _bitmap = new Bitmap(imageStream);
+            return Resize2ImageStream(Width, imageStream, _bitmap.RawFormat);
+        }
+        public Stream Resize2ImageStream(int Width, Stream imageStream, ImageFormat imageformat)
+        {
+            Bitmap _bitmap = new Bitmap(imageStream);
+            Stream _stm = new MemoryStream();
+            Resize(Width, _bitmap).Save(_stm, imageformat);
+            return _stm;
+        }
+        public Stream Resize2ImageStream(int Width, int Height, Stream imageStream, bool Proportionably = true)
+        {
+            Bitmap _bitmap = new Bitmap(imageStream);
+            return Resize2ImageStream(Width, Height, imageStream, _bitmap.RawFormat, Proportionably);
+        }
+        public Stream Resize2ImageStream(int Width, int Height, Stream imageStream, ImageFormat imageformat, bool Proportionably = true)
+        {
+            MemoryStream _stream = new MemoryStream();
+            using (Bitmap _bitmap = new Bitmap(imageStream))
+            {
+                using (Bitmap _resizedbitmap = Resize(Width, Height, _bitmap, Proportionably))
+                {
+                    _resizedbitmap.Save(_stream, imageformat);
+                }
+            }
+            return _stream;
+        }
+        #endregion
+
+        #region from file
+        public Stream Resize2ImageStream(int Width, string filepath)
+        {
+            Stream _stm = new MemoryStream();
+            using (Bitmap _bitmap = Resize(Width, filepath))
+            {
+                Resize(Width, filepath).Save(_stm, _bitmap.RawFormat);
+            }
+            return _stm;
+        }
+        public Stream Resize2ImageStream(int Width, string filepath, ImageFormat imageformat)
+        {
+            Stream _stm = new MemoryStream();
+            Resize(Width, filepath).Save(_stm, imageformat);
+            return _stm;
+        }
+        public Stream Resize2ImageStream(int Width, int Height, string filepath, bool Proportionably = true)
+        {
+            Stream _stm = new MemoryStream();
+            using (Bitmap _bitmap = Resize(Width, Height, filepath, Proportionably))
+            {
+                Resize(Width, Height, filepath, Proportionably).Save(_stm, _bitmap.RawFormat);
+            }
+            return _stm;
+        }
+        public Stream Resize2ImageStream(int Width, int Height, string filepath, ImageFormat imageformat, bool Proportionably = true)
+        {
+            Stream _stm = new MemoryStream();
+            Resize(Width, Height, filepath, Proportionably).Save(_stm, imageformat);
+            return _stm;
+        }
+        #endregion
+
+        #endregion
+
+        #region resize and save as file
+        public void Resize2File(int Width, string FilePath, string resizedFilePath)
+        {
+            using (Bitmap Resized = Resize(Width, FilePath))
+            {
+                SaveToFile(resizedFilePath, Resized.RawFormat, Resized);
+            }
+        }
+
+        public void Resize2FileByRecommendJPEG(int Width, string FilePath, string resizedFilePath)
+        {
+            using (Bitmap Resized = Resize(Width, FilePath))
+            {
+                SaveToFile(resizedFilePath, ImageFormat.Jpeg, Resized, true);
+            }
+        }
+
+        public void Resize2File(int Width, string FilePath, string resizedFilePath, ImageFormat ImgFormat)
+        {
+            using (Bitmap Resized = Resize(Width, FilePath))
+            {
+                SaveToFile(resizedFilePath, ImgFormat, Resized);
+            }
+        }
+
+        public void Resize2File(int Width, int Height, string FilePath, string resizedFilePath, bool Proportionably = true)
+        {
+            using (Bitmap Resized = Resize(Width, Height, FilePath, Proportionably))
+            {
+                SaveToFile(resizedFilePath, Resized.RawFormat, Resized);
+            }
+        }
+
+        public void Resize2FileByRecommendJPEG(int Width, int Height, string FilePath, string resizedFilePath, bool Proportionably = true)
+        {
+            using (Bitmap Resized = Resize(Width, Height, FilePath, Proportionably))
+            {
+                SaveToFile(resizedFilePath, ImageFormat.Jpeg, Resized, true);
+            }
+        }
+
+        public void Resize2File(int Width, int Height, string FilePath, string resizedFilePath, ImageFormat ImgFormat, bool Proportionably = true)
+        {
+            using (Bitmap Resized = Resize(Width, Height, FilePath, Proportionably))
+            {
+                SaveToFile(resizedFilePath, ImgFormat, Resized);
+            }
+        }
+        #endregion
     }
 }
